589b26358cd151b218fa33bdc24d873d72394caf
iByte <ibyteinfomatics.com>
2023-09-07 17:31:38 +0530

    update code after changes the env file

--- next.config.js
+++ next.config.js
@@ -1,11 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 
-  env: {
+  env: (process.env.NODE_ENV === 'development' ? {
     API_BASE_URL: 'https://reqres.in/api',
     GOOGLE_CLIENT_ID: '864983917151-sl5tctj5r09q68itb2ki6du5kldm75ds.apps.googleusercontent.com',
-    GOOGLE_CLIENT_SECRET: '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'
-  },
+    GOOGLE_CLIENT_SECRET: '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',
+    AUTH_REDIRECT_URL: "http://localhost:3000"
+  } : {
+    API_BASE_URL: 'https://reqres.in/api',
+    GOOGLE_CLIENT_ID: '864983917151-sl5tctj5r09q68itb2ki6du5kldm75ds.apps.googleusercontent.com',
+    GOOGLE_CLIENT_SECRET: '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',
+    AUTH_REDIRECT_URL: "https://silver-snickerdoodle-77bd0a.netlify.app"
+  }),
   images: {
     remotePatterns: [
       {
@@ -15,9 +21,9 @@ const nextConfig = {
         pathname: '/account123/**',
       },
     ],
-    domains:['lh3.googleusercontent.com']
-   
-   
+    domains: ['lh3.googleusercontent.com']
+
+
   },
 
 }
